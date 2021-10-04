f5b2d8662a1ee5c973b59b6198336951a066b8df
Yescii <cgonzalez.jesse@gmail.com>
2021-10-04 17:29:57 -0400

    routes folder

---
+++ controllers/store.js
@@ -0,0 +1,39 @@
+// Dependencies
+const express = require("express");
+const storeRouter = express.Router();
+const Store = require("../models/StoreDB.js");
+
+storeItems = [
+  {
+    name: "bread",
+    discription: "Mixed with natural milk or water, made into a dough",
+    img: "https://images.heb.com/is/image/HEBGrocery/001539558",
+    price: 1.99,
+    qty: 45,
+  },
+  {
+    name: "peanut butter",
+    discription: "Mixed with natural milk or water, made into a dough",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsqxV0R7pwZKX7FX-n7cnUorCCK9wUcX2wZg&usqp=CAU",
+    price: 1.5,
+    qty: 30,
+  },
+  {
+    name: "24 Water Pack",
+    discription: "Pack of 24 water bottles",
+    img: "https://i.ebayimg.com/images/g/RNwAAOSwmjdesUU5/s-l640.jpg",
+    price: 3.25,
+    qty: 56,
+  },
+];
+
+//INDEX
+storeRouter.get("/", (req, res) => {
+  Store.find({}, (error, allItems) => {
+    res.render("index.ejs", {
+      items: allItems,
+    });
+  });
+});
+
+module.exports = storeRouter;
